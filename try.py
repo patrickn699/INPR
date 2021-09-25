af12dd5ab07a4a693a9bd8ea18e7da510c1a6b60
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-09-25 17:44:27 +0530

    images added

--- try.py
+++ try.py
@@ -1,8 +1,13 @@
+import cv2
+from PIL import Image
+
 from INPR import detect_plates
 import matplotlib.pyplot as plt
 
-im = 'test_img/bm.jpg'
+im = 'test_img/kia.jpg'
 
 grap,op,img = detect_plates(im)
 plt.imshow(grap)
-plt.show() 
+plt.savefig('grap3.png')
+plt.show()
+#plt.show() 
