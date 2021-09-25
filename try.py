d47b662dfac2ac503aca1cc523faf420ed9ffdb5
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-09-25 20:22:57 +0530

    readme updated

--- try.py
+++ try.py
@@ -1,13 +1,17 @@
 import cv2
 from PIL import Image
 
-from INPR import detect_plates
+from INPR import detect_plates,fetch_details
 import matplotlib.pyplot as plt
 
 im = 'test_img/kia.jpg'
 
 grap,op,img = detect_plates(im)
-plt.imshow(grap)
-plt.savefig('grap3.png')
-plt.show()
+#plt.imshow(grap)
+#plt.savefig('grap3.png')
+#print(op)
+#print(img)
 #plt.show() 
+
+num_plate_text = fetch_details(op,img)
+print(num_plate_text)
