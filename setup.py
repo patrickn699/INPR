76d5549972c6eaff4304e3f701720e1ffec34722
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-09-25 21:39:40 +0530

    setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 
 with open("requirements.txt",'r') as f:
     requirements = f.read().splitlines()
-    #print(requirements)
 
-
+with open("README.md",'r') as f:
+    long_description = f.read()
 
 
 setup(
@@ -14,6 +14,7 @@ setup(
     version='0.1',
     description='A deep learning based project to detect indian number plates',
     author='Prathmesh Patil',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages= setuptools.find_packages(),
     keywords=['Deep Learning','Computer Vision','Pytorch','Detectron2'],
@@ -24,6 +25,6 @@ setup(
     ],
     python_requires='>=3.6',
     py_modules=['INPR'],
-    #install_requires=requirements
+    install_requires=requirements
     
 )
